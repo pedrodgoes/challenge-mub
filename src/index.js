2d9ac3c7e67b86edb2508f725528c672fe4e7758
Pedro Góes <pedrogoesbj@gmail.com>
2022-08-05 02:52:36 -0400

    core: install functional dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import './styles/global.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Fragment>
+      <Home />
+      <ToastContainer />
+    </Fragment>
   </React.StrictMode>
 );
