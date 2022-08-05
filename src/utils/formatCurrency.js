d41222cc750ceb3404e40c59de89c75a22574b30
Pedro Góes <pedrogoesbj@gmail.com>
2022-08-05 02:57:55 -0400

    feat: create function to format money

---
+++ src/utils/formatCurrency.js
@@ -0,0 +1,7 @@
+export const formatCurrency = ( value ) => {
+  const currency = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(value)
+  return currency;
+}
